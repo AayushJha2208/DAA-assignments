73964bf00517dab3c61592b75bb0953445d66870
AayushJha2208 <aayushjha2208@gmail.com>
2026-01-31 16:04:14 +0530

    Update A1Q4_recursive.cpp

--- A1/A1Q4_recursive.cpp
+++ A1/A1Q4_recursive.cpp
@@ -9,34 +9,36 @@
 #include <cmath>
 #include <climits>
 using namespace std;
-int crossum(int *arr,int l,int h,int m){
-    int sum=0;
-    int lsum=INT_MIN,rsum=INT_MIN;
-    int i;
-    for(i=m;i>=l;i--){
-        sum+=arr[i];
-        if(sum>lsum)lsum=sum;
+int crossum(int *arr, int l, int h, int m) {
+    int sum = 0;
+    int lsum = INT_MIN, rsum = INT_MIN;
+
+    for (int i = m; i >= l; i--) {
+        sum += arr[i];
+        lsum = max(lsum, sum);
     }
-    sum=0;
-    for(i=m+1;i<=h;i++){
-        sum+=arr[i];
-        if(sum>rsum)rsum=sum;
+
+    sum = 0;
+    for (int i = m + 1; i <= h; i++) {
+        sum += arr[i];
+        rsum = max(rsum, sum);
     }
-    return max(rsum,0)+max(lsum,0);
+
+    return lsum + rsum;
 }
 int subsum(int *arr,int l,int h){
-    if(l>h) return 0;
     if(l==h) return arr[l];
     int m=(l+h)/2;
-    int lt=subsum(arr,l,m-1);
+    int lt=subsum(arr,l,m);
     int rt=subsum(arr,m+1,h);
     int cr=crossum(arr,l,h,m);
-    if (lt>rt && lt>cr)return lt;
-    else if(rt>lt && rt>cr) return rt;
-    else return cr;
+    // if (lt>rt && lt>cr)return lt;
+    // else if(rt>lt && rt>cr) return rt;
+    // else return cr;
+    return max(max(lt,rt),cr);
 }
 int main(){
-    int arr[]={2, 3, 4, 5, 7};
-    cout<<"Maximum subarray sum : "<<subsum(arr,0,4);
+    int arr[] = {-2, -1};
+    cout<<"Maximum subarray sum : "<<subsum(arr,0,1);
     return 0;
 }
